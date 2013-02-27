4c156f93a2359298bc14c741598bad916b11faa1
Grant Garcia <github@grantgarcia.org>
2013-02-27 16:45:18 -0700

    Added fix for checkbox selection
    This sets the clones' checked *attribute* values to the corresponding *property* values. The result is correct onscreen and in POST data, but the DOM of the clone is not identical to the source DOM.

--- jquery.fix.clone.js
+++ jquery.fix.clone.js
@@ -10,14 +10,17 @@
 
 (function (original) {
   jQuery.fn.clone = function () {
-    var result           = original.apply(this, arguments),
-        my_textareas     = this.find('textarea').add(this.filter('textarea')),
-        result_textareas = result.find('textarea').add(result.filter('textarea')),
-        my_selects       = this.find('select').add(this.filter('select')),
-        result_selects   = result.find('select').add(result.filter('select'));
+    var result            = original.apply(this, arguments),
+        my_textareas      = this.find('textarea').add(this.filter('textarea')),
+        result_textareas  = result.find('textarea').add(result.filter('textarea')),
+        my_selects        = this.find('select').add(this.filter('select')),
+        result_selects    = result.find('select').add(result.filter('select')),
+        my_checkboxes     = this.find('input[type=checkbox]').add(this.filter('input[type=checkbox]')),
+        result_checkboxes = result.find('input[type=checkbox]').add(result.filter('input[type=checkbox]'));
 
-    for (var i = 0, l = my_textareas.length; i < l; ++i) $(result_textareas[i]).val($(my_textareas[i]).val());
-    for (var i = 0, l = my_selects.length;   i < l; ++i) result_selects[i].selectedIndex = my_selects[i].selectedIndex;
+    for (var i = 0, l = my_textareas.length;  i < l; ++i) $(result_textareas[i]).val($(my_textareas[i]).val());
+    for (var i = 0, l = my_selects.length;    i < l; ++i) result_selects[i].selectedIndex = my_selects[i].selectedIndex;
+    for (var i = 0, l = my_checkboxes.length; i < l; ++i) $(result_checkboxes[i]).attr('checked', my_checkboxes[i].checked);
 
     return result;
   };
